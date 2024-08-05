253050ab40e2a949a74c140c36f72349c788dab3
SilverxSoul <8failure@gmail.com>
2024-08-05 11:30:11 +0700

    change speed make conflict

--- Assets/Script/NinjaGame/PlayerController.cs
+++ Assets/Script/NinjaGame/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 7f;
     private Animator animator;
     private PlayerStat playerstat;
     public GameObject ProjectilePrefab;
