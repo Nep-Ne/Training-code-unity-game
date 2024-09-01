c7e5d720c93848a9791d53aa9249da66f8a24f5d
SilverxSoul <8failure@gmail.com>
2024-09-01 08:35:44 +0700

    Pathfinding for enemy

--- Assets/Script/NinjaGame/EnemyController.cs
+++ Assets/Script/NinjaGame/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;//neu bo dong nay va doi tat ca NavMeshAgent thanh UnityEngine.AI.NavMeshAgent thi bi loi !! Tai Sao??
 
 public class EnemyController : MonoBehaviour
 {
@@ -10,22 +11,30 @@ public class EnemyController : MonoBehaviour
     private Vector2 direction;
     private Rigidbody2D rb;
     private Animator animator;
-    public float speed = 4f;
+    //public float speed = 4f;
+    //speed da duoc su dung trong NevMeshAgent !!
     public float health = 10f;
     public float maxhealth = 10f;
     [SerializeField] private GameObject floatingText;
+    NavMeshAgent agent;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         Player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if(agent)//kiem tra khac null vi khi death no se bi delete nghia la Null, neu khong xet truong hop NULL thi se bi loi NullReference !!
+        agent.SetDestination(Player.position);
     }
 
     private void FixedUpdate()
@@ -35,10 +44,12 @@ public class EnemyController : MonoBehaviour
     }
 
     void Chase(Transform target)
-    { 
-        float distance = Vector2.Distance(target.position, transform.position);
-        direction = new Vector2(target.position.x - transform.position.x, target.position.y - transform.position.y);
-        rb.velocity = direction.normalized* speed ;
+    {
+
+        //float distance = Vector2.Distance(target.position, transform.position);
+        //direction = new Vector2(target.position.x - transform.position.x, target.position.y - transform.position.y);
+        //rb.velocity = direction.normalized* speed ;
+        direction = new Vector2(agent.velocity.x, agent.velocity.y);
         animator.SetFloat("DirectionX", direction.x);
         animator.SetFloat("DirectionY", direction.y);
         animator.SetBool("Moving", true);
@@ -48,11 +59,14 @@ public class EnemyController : MonoBehaviour
     {
         animator.SetBool("Death", true);
         Debug.Log("Death");
+        //xoa AI
+        Destroy(gameObject.GetComponent<NavMeshAgent>());
         //lam cho ke thu ko con collider de nguoi choi co the di qua animation death !!
         Destroy(gameObject.GetComponent<Collider2D>());
 
         //lam cho ke thu khong bi vang di sau khi bi dinh dan
         Destroy(gameObject.GetComponent<Rigidbody2D>());
+        
         
     }
     public void TakeDamage(float dmg)
